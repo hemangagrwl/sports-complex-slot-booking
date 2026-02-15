6d8c4b4b76a1918561d822bc1346eccdfd84c89a
Hemang Agarwal <hemang.agarwal2004@gmail.com>
2026-02-15 18:09:54 +0530

    Force redeploy

--- app.py
+++ app.py
@@ -7,10 +7,6 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-
-app = Flask(__name__)
-app.secret_key = 'marenasecretkey'  # Change this to a secure random key
-
 # ---------------- MySQL Connection ----------------
 app = Flask(__name__)
 app.secret_key = os.getenv("SECRET_KEY")
@@ -459,4 +455,4 @@ def logout():
     return redirect(url_for('login'))
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    (host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
