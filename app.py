e5b13ebd80496782091717feab0fd6d8ee85f26e
Hemang Agarwal <hemang.agarwal2004@gmail.com>
2026-02-15 18:13:47 +0530

    Fix syntax error in app.run block

--- app.py
+++ app.py
@@ -455,4 +455,4 @@ def logout():
     return redirect(url_for('login'))
 
 if __name__ == "__main__":
-    (host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
